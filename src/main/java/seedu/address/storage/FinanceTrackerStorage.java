d507cf08f7f7b263c0617006fbe97d6d54c4eb85
LAPTOP-3IPPG5K7\Jhocx <jhocx@u.nus.edu>
2019-02-27 13:58:00 +0800

    Rename Process.
    AddressBookStorage Interface done.

---
+++ src/main/java/seedu/address/storage/FinanceTrackerStorage.java
@@ -0,0 +1,45 @@
+package seedu.address.storage;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.model.ReadOnlyFinanceTracker;
+
+/**
+ * Represents a storage for {@link seedu.address.model.FinanceTracker}.
+ */
+public interface FinanceTrackerStorage {
+
+    /**
+     * Returns the file path of the data file.
+     */
+    Path getFinanceTrackerFilePath();
+
+    /**
+     * Returns FinanceTracker data as a {@link ReadOnlyFinanceTracker}.
+     *   Returns {@code Optional.empty()} if storage file is not found.
+     * @throws DataConversionException if the data in storage is not in the expected format.
+     * @throws IOException if there was any problem when reading from the storage.
+     */
+    Optional<ReadOnlyFinanceTracker> readFinanceTracker() throws DataConversionException, IOException;
+
+    /**
+     * @see #getFinanceTrackerFilePath()
+     */
+    Optional<ReadOnlyFinanceTracker> readFinanceTracker(Path filePath) throws DataConversionException, IOException;
+
+    /**
+     * Saves the given {@link ReadOnlyFinanceTracker} to the storage.
+     * @param financeTracker cannot be null.
+     * @throws IOException if there was any problem writing to the file.
+     */
+    void saveFinanceTracker(ReadOnlyFinanceTracker financeTracker) throws IOException;
+
+    /**
+     * @see #saveFinanceTracker(ReadOnlyFinanceTracker)
+     */
+    void saveFinanceTracker(ReadOnlyFinanceTracker financeTracker, Path filePath) throws IOException;
+
+}
