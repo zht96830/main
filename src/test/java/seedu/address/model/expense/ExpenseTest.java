45e1e58b688b112e3122ef3a91501542ea966b7e
gohyinhao <yinhao.g@gmail.com>
2019-03-10 13:34:38 +0800

    Fix ExpenseTest

--- src/test/java/seedu/address/model/expense/ExpenseTest.java
+++ src/test/java/seedu/address/model/expense/ExpenseTest.java
@@ -1,11 +1,15 @@
-package seedu.address.model.person;
+package seedu.address.model.expense;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_AMOUNT_DEBT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_AMOUNT_EXPENSE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_CATEGORY_DEBT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_CATEGORY_EXPENSE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_EXPENSE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DEADLINE_DEBT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_DEBT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_EXPENSE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARKS_EXPENSE;
 import static seedu.address.testutil.TypicalExpenses.DOCTOR;
 import static seedu.address.testutil.TypicalExpenses.DUCK_RICE;
@@ -14,7 +18,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.model.attributes.Amount;
+import seedu.address.model.attributes.Category;
+import seedu.address.model.attributes.Date;
+import seedu.address.model.attributes.Name;
 import seedu.address.model.expense.Expense;
+import seedu.address.testutil.Assert;
 import seedu.address.testutil.ExpenseBuilder;
 
 public class ExpenseTest {
@@ -22,50 +31,49 @@ public class ExpenseTest {
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void asObservableList_modifyList_throwsUnsupportedOperationException() {
-        Expense expense = new ExpenseBuilder().build();
-        thrown.expect(UnsupportedOperationException.class);
-        expense.getRemarks();
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () ->
+                new Expense(null, new Amount("1"), new Date("01-01-2018"),
+                        Category.valueOf("FOOD"), null));
+        Assert.assertThrows(NullPointerException.class, () ->
+                new Expense(new Name("rice"), null, new Date("01-01-2018"),
+                        Category.valueOf("FOOD"), null));
+        Assert.assertThrows(NullPointerException.class, () ->
+                new Expense(new Name("rice"), new Amount("1"), new Date("01-01-2018"),
+                        null, null));
     }
 
     @Test
-    public void isSamePerson() {
+    public void isSameExpense() {
         // same object -> returns true
         assertTrue(DUCK_RICE.isSameExpense(DUCK_RICE));
 
         // null -> returns false
         assertFalse(DUCK_RICE.isSameExpense(null));
 
-        // different phone and email -> returns false
-        Expense editedAlice = new ExpenseBuilder(DUCK_RICE).withAmount(VALID_AMOUNT_DEBT)
-                .withDate(VALID_CATEGORY_DEBT).build();
-        assertFalse(DUCK_RICE.isSameExpense(editedAlice));
-
         // different name -> returns false
-        editedAlice = new ExpenseBuilder(DUCK_RICE).withName(VALID_NAME_DEBT).build();
-        assertFalse(DUCK_RICE.isSameExpense(editedAlice));
+        Expense editedExpense = new ExpenseBuilder(DUCK_RICE).withName(VALID_NAME_EXPENSE).build();
+        assertFalse(DUCK_RICE.isSameExpense(editedExpense));
 
-        // same name, same phone, different attributes -> returns true
-        editedAlice = new ExpenseBuilder(DUCK_RICE).withDate(VALID_CATEGORY_DEBT).withCategory(VALID_DEADLINE_DEBT)
-                .withRemarks(VALID_REMARKS_EXPENSE).build();
-        assertTrue(DUCK_RICE.isSameExpense(editedAlice));
+        // different amount -> returns false
+        editedExpense = new ExpenseBuilder(DUCK_RICE).withAmount(VALID_AMOUNT_EXPENSE).build();
+        assertFalse(DUCK_RICE.isSameExpense(editedExpense));
 
-        // same name, same email, different attributes -> returns true
-        editedAlice = new ExpenseBuilder(DUCK_RICE).withAmount(VALID_AMOUNT_DEBT).withCategory(VALID_DEADLINE_DEBT)
-                .withRemarks(VALID_REMARKS_EXPENSE).build();
-        assertTrue(DUCK_RICE.isSameExpense(editedAlice));
+        // different date -> returns false
+        editedExpense = new ExpenseBuilder(DUCK_RICE).withDate(VALID_DATE_EXPENSE).build();
+        assertFalse(DUCK_RICE.isSameExpense(editedExpense));
 
-        // same name, same phone, same email, different attributes -> returns true
-        editedAlice = new ExpenseBuilder(DUCK_RICE).withCategory(VALID_DEADLINE_DEBT)
+        // same name, same amount, same date, different attributes -> returns true
+        editedExpense = new ExpenseBuilder(DUCK_RICE).withCategory(VALID_CATEGORY_EXPENSE)
                 .withRemarks(VALID_REMARKS_EXPENSE).build();
-        assertTrue(DUCK_RICE.isSameExpense(editedAlice));
+        assertTrue(DUCK_RICE.isSameExpense(editedExpense));
     }
 
     @Test
     public void equals() {
         // same values -> returns true
-        Expense aliceCopy = new ExpenseBuilder(DUCK_RICE).build();
-        assertTrue(DUCK_RICE.equals(aliceCopy));
+        Expense expenseCopy = new ExpenseBuilder(DUCK_RICE).build();
+        assertTrue(DUCK_RICE.equals(expenseCopy));
 
         // same object -> returns true
         assertTrue(DUCK_RICE.equals(DUCK_RICE));
@@ -80,23 +88,23 @@ public class ExpenseTest {
         assertFalse(DUCK_RICE.equals(DOCTOR));
 
         // different name -> returns false
-        Expense editedAlice = new ExpenseBuilder(DUCK_RICE).withName(VALID_NAME_DEBT).build();
-        assertFalse(DUCK_RICE.equals(editedAlice));
+        Expense editedExpense = new ExpenseBuilder(DUCK_RICE).withName(VALID_NAME_EXPENSE).build();
+        assertFalse(DUCK_RICE.equals(editedExpense));
 
-        // different phone -> returns false
-        editedAlice = new ExpenseBuilder(DUCK_RICE).withAmount(VALID_AMOUNT_DEBT).build();
-        assertFalse(DUCK_RICE.equals(editedAlice));
+        // different amount -> returns false
+        editedExpense = new ExpenseBuilder(DUCK_RICE).withAmount(VALID_AMOUNT_EXPENSE).build();
+        assertFalse(DUCK_RICE.equals(editedExpense));
 
-        // different email -> returns false
-        editedAlice = new ExpenseBuilder(DUCK_RICE).withDate(VALID_CATEGORY_DEBT).build();
-        assertFalse(DUCK_RICE.equals(editedAlice));
+        // different date -> returns false
+        editedExpense = new ExpenseBuilder(DUCK_RICE).withDate(VALID_DATE_EXPENSE).build();
+        assertFalse(DUCK_RICE.equals(editedExpense));
 
-        // different address -> returns false
-        editedAlice = new ExpenseBuilder(DUCK_RICE).withCategory(VALID_DEADLINE_DEBT).build();
-        assertFalse(DUCK_RICE.equals(editedAlice));
+        // different category -> returns false
+        editedExpense = new ExpenseBuilder(DUCK_RICE).withCategory("OTHERS").build();
+        assertFalse(DUCK_RICE.equals(editedExpense));
 
-        // different tags -> returns false
-        editedAlice = new ExpenseBuilder(DUCK_RICE).withRemarks(VALID_REMARKS_EXPENSE).build();
-        assertFalse(DUCK_RICE.equals(editedAlice));
+        // different remarks -> returns false
+        editedExpense = new ExpenseBuilder(DUCK_RICE).withRemarks(VALID_REMARKS_EXPENSE).build();
+        assertFalse(DUCK_RICE.equals(editedExpense));
     }
 }
--- src/test/java/seedu/address/model/expense/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/expense/NameContainsKeywordsPredicateTest.java
@@ -1,4 +1,4 @@
-package seedu.address.model.person;
+package seedu.address.model.expense;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.testutil.ExpenseBuilder;
 
 public class NameContainsKeywordsPredicateTest {
