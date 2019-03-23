d3a7d96b00435d511dad2c156ac7d3696f074975
jamessspanggg <jamespangmunwai@gmail.com>
2019-03-24 01:19:56 +0800

    Add tests for ListDebt and ListRecurring, Update BrowserPanelTest

---
+++ src/test/java/seedu/address/logic/commands/debtcommands/ListDebtCommandTest.java
@@ -0,0 +1,60 @@
+package seedu.address.logic.commands.debtcommands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showDebtAtIndex;
+import static seedu.address.testutil.TypicalDebts.getTypicalFinanceTrackerWithDebts;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EXPENSE;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.attributes.View;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListDebtCommand.
+ */
+public class ListDebtCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalFinanceTrackerWithDebts(), new UserPrefs());
+        expectedModel = new ModelManager(model.getFinanceTracker(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListDebtCommand(View.ALL), model, commandHistory,
+                String.format(ListDebtCommand.MESSAGE_SUCCESS, View.ALL), expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showDebtAtIndex(model, INDEX_FIRST_EXPENSE);
+        assertCommandSuccess(new ListDebtCommand(View.ALL), model, commandHistory,
+                String.format(ListDebtCommand.MESSAGE_SUCCESS, View.ALL), expectedModel);
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsHealthCareCategory() {
+        expectedModel.updateFilteredDebtList(Model.PREDICATE_SHOW_HEALTHCARE_DEBTS);
+        assertCommandSuccess(new ListDebtCommand(View.HEALTHCARE), model, commandHistory,
+                String.format(ListDebtCommand.MESSAGE_SUCCESS, View.HEALTHCARE), expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsHealthCareCategory() {
+        expectedModel.updateFilteredDebtList(Model.PREDICATE_SHOW_HEALTHCARE_DEBTS);
+        showDebtAtIndex(model, INDEX_FIRST_EXPENSE);
+        assertCommandSuccess(new ListDebtCommand(View.HEALTHCARE), model, commandHistory,
+                String.format(ListDebtCommand.MESSAGE_SUCCESS, View.HEALTHCARE), expectedModel);
+    }
+
+}
--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -2,7 +2,10 @@ package seedu.address.ui;
 
 import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
 import static org.junit.Assert.assertEquals;
+import static seedu.address.testutil.TypicalBudgets.FOOD_BUDGET;
+import static seedu.address.testutil.TypicalDebts.AMY;
 import static seedu.address.testutil.TypicalExpenses.DUCK_RICE;
+import static seedu.address.testutil.TypicalRecurrings.PHONE_BILL;
 
 import java.net.URL;
 
@@ -40,17 +43,63 @@ public class BrowserPanelTest extends GuiUnitTest {
         // default web page
         assertEquals(BrowserPanel.DEFAULT_PAGE, browserPanelHandle.getLoadedUrl());
 
-        // associated web page of a expense
+        // associated web page of an expense
         guiRobot.interact(() -> selectedExpense.set(DUCK_RICE));
         String urlQuery = BrowserPanel.QUESTION_MARK + BrowserPanel.QUERY_NAME + DUCK_RICE.getName().name
-                + BrowserPanel.QUERY_CATEGORY + DUCK_RICE.getCategory().toString() + BrowserPanel.QUERY_AMOUNT
-                + BrowserPanel.DOLLAR_SIGN + DUCK_RICE.getAmount().toString() + BrowserPanel.QUERY_DATE
-                + DUCK_RICE.getDate().toString() + BrowserPanel.QUERY_REMARK + DUCK_RICE.getRemarks();
+                + BrowserPanel.QUERY_CATEGORY + DUCK_RICE.getCategory().toString()
+                + BrowserPanel.QUERY_AMOUNT + BrowserPanel.DOLLAR_SIGN + DUCK_RICE.getAmount().toString()
+                + BrowserPanel.QUERY_DATE + DUCK_RICE.getDate().toString()
+                + BrowserPanel.QUERY_REMARK + DUCK_RICE.getRemarks();
 
         URL expectedExpenseUrl = new URL(BrowserPanel.EXPENSES_PAGE_URL
                 + urlQuery.replaceAll(" ", "%20"));
 
         waitUntilBrowserLoaded(browserPanelHandle);
         assertEquals(expectedExpenseUrl, browserPanelHandle.getLoadedUrl());
+
+        // associated web page of a debt
+        guiRobot.interact(() -> selectedDebt.set(AMY));
+        urlQuery = BrowserPanel.QUESTION_MARK + BrowserPanel.QUERY_PERSONOWED + AMY.getPersonOwed().name
+                + BrowserPanel.QUERY_CATEGORY + AMY.getCategory().toString()
+                + BrowserPanel.QUERY_AMOUNT + BrowserPanel.DOLLAR_SIGN + AMY.getAmount().toString()
+                + BrowserPanel.QUERY_DEADLINE + AMY.getDeadline().toString()
+                + BrowserPanel.QUERY_REMARK + AMY.getRemarks();
+
+        URL expectedDebtUrl = new URL(BrowserPanel.DEBTS_PAGE_URL
+                + urlQuery.replaceAll(" ", "%20"));
+
+        waitUntilBrowserLoaded(browserPanelHandle);
+        assertEquals(expectedDebtUrl, browserPanelHandle.getLoadedUrl());
+
+        // associated web page of a debt
+        guiRobot.interact(() -> selectedBudget.set(FOOD_BUDGET));
+        urlQuery = BrowserPanel.QUESTION_MARK
+                + BrowserPanel.QUERY_CATEGORY + FOOD_BUDGET.getCategory().toString()
+                + BrowserPanel.QUERY_AMOUNT + BrowserPanel.DOLLAR_SIGN + FOOD_BUDGET.getAmount().toString()
+                + BrowserPanel.QUERY_STARTDATE + FOOD_BUDGET.getStartDate().toString()
+                + BrowserPanel.QUERY_ENDDATE + FOOD_BUDGET.getEndDate().toString()
+                + BrowserPanel.QUERY_REMARK + FOOD_BUDGET.getRemarks();
+
+        URL expectedBudgetUrl = new URL(BrowserPanel.BUDGETS_PAGE_URL
+                + urlQuery.replaceAll(" ", "%20"));
+
+        waitUntilBrowserLoaded(browserPanelHandle);
+        assertEquals(expectedBudgetUrl, browserPanelHandle.getLoadedUrl());
+
+        // associated web page of a recurring
+        guiRobot.interact(() -> selectedRecurring.set(PHONE_BILL));
+        urlQuery = BrowserPanel.QUESTION_MARK + BrowserPanel.QUERY_NAME + PHONE_BILL.getName().name
+                + BrowserPanel.QUERY_CATEGORY + PHONE_BILL.getCategory().toString()
+                + BrowserPanel.QUERY_AMOUNT + BrowserPanel.DOLLAR_SIGN + PHONE_BILL.getAmount().toString()
+                + BrowserPanel.QUERY_DATE + PHONE_BILL.getDate().toString()
+                + BrowserPanel.QUERY_FREQUENCY + PHONE_BILL.getFrequency()
+                + BrowserPanel.QUERY_OCCURENCE + PHONE_BILL.getOccurrence()
+                + BrowserPanel.QUERY_REMARK + PHONE_BILL.getRemarks();
+
+        URL expectedRecurringUrl = new URL(BrowserPanel.RECURRINGS_PAGE_URL
+                + urlQuery.replaceAll(" ", "%20"));
+
+        waitUntilBrowserLoaded(browserPanelHandle);
+        assertEquals(expectedRecurringUrl, browserPanelHandle.getLoadedUrl());
     }
 }
