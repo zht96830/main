904958710d1dfb9c5a690c1e36075f9412261849
zht96830 <zhihui_teoh@live.com>
2019-04-01 21:27:06 +0800

    Create Statistic Class

---
+++ src/main/java/seedu/address/model/Statistics.java
@@ -0,0 +1,18 @@
+package seedu.address.model;
+
+import javafx.collections.transformation.FilteredList;
+import seedu.address.model.attributes.Category;
+import seedu.address.model.attributes.Date;
+import seedu.address.model.budget.Budget;
+import seedu.address.model.debt.Debt;
+import seedu.address.model.expense.Expense;
+
+public class Statistics {
+
+    public static void calculateStats(Date startDate, Date endDate, Category category
+            , FilteredList<Expense> statsExpenses, FilteredList<Debt> statsDebts
+            , FilteredList<Budget> statsBudgets){
+
+    }
+
+}
